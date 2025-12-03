25ed05befcb79d2d3a36aac5fbc4a8bd8caaf6df
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2025-12-03 12:30:40 -0300

    Day 2 - Part 1

---
+++ day_02/src/main.rs
@@ -0,0 +1,82 @@
+use anyhow::{self, Context};
+use xmas::display_result;
+
+fn main() -> anyhow::Result<()> {
+    part_1()?;
+    println!();
+    part_2()?;
+    Ok(())
+}
+
+fn part_1() -> anyhow::Result<()> {
+    println!("Part 1:");
+    let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
+
+    let result = add_invalid_ids(&input);
+
+    display_result(&result);
+    Ok(())
+}
+
+fn part_2() -> anyhow::Result<()> {
+    println!("Part 2:");
+
+    Ok(())
+}
+
+fn add_invalid_ids(input: &str) -> u64 {
+    let mut sum = 0;
+
+    for range_input in input.trim().split(',') {
+        let (from_s, to_s) = range_input.trim().split_once('-').unwrap();
+        let range = from_s.parse::<u64>().unwrap()..=to_s.parse::<u64>().unwrap();
+
+        for n in range {
+            if !is_valid(n) {
+                // println!("{}: {}", range_input, n);
+                sum += n;
+            }
+        }
+    }
+
+    sum
+}
+
+fn is_valid(id: u64) -> bool {
+    let s = id.to_string();
+    let len: usize = s.len();
+
+    if len % 2 != 0 {
+        return true;
+    }
+
+    for pattern_len in (len / 2)..=(len / 2) {
+        if pattern_len == 0 {
+            continue;
+        }
+
+        if len % pattern_len != 0 {
+            continue;
+        }
+
+        let mut is_repeating = true;
+
+        let pattern = &s[0..pattern_len];
+        let repeats = len / pattern_len;
+        for i in 1..repeats {
+            let offset = i * pattern_len;
+            let current_section = &s[offset..(offset + pattern_len)];
+            
+            if pattern != current_section {
+                is_repeating = false;
+                break;
+            }
+        }
+
+        if is_repeating {
+            return false;
+        }
+    }
+
+    true
+}
