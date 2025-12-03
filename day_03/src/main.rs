39d3f41148abfbf325c0be2988e9165ec4e3ca77
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2025-12-03 15:34:42 -0300

    Cleaned up code

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -10,6 +10,10 @@ fn main() -> anyhow::Result<()> {
 
 fn part_1() -> anyhow::Result<()> {
     println!("Part 1:");
+    let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
+
+    let result: u64 = input.lines().map(|l| maximize_joltage(l, 2)).sum();
+    display_result(&result);
     Ok(())
 }
 
@@ -17,35 +21,9 @@ fn part_2() -> anyhow::Result<()> {
     println!("Part 2:");
     let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
 
-    let mut result = 0;
-    for line in input.lines() {
-        let biggest = maximize_joltage(&line, 12);
-        // println!("{line} => {biggest}");
-        result += biggest;
-    }
-
+    let result: u64 = input.lines().map(|l| maximize_joltage(l, 12)).sum();
     display_result(&result);
     Ok(())
-}
-
-fn get_biggest_joltage(input: &str) -> u64 {
-    let mut biggest_found: Option<(usize, u64)> = None;
-    for (i, ch) in input[..input.len() - 1].chars().enumerate() {
-        let digit = ch.to_digit(10).unwrap() as u64;
-
-        if biggest_found.is_none_or(|n| n.1 < digit) {
-            biggest_found = Some((i, digit))
-        }
-    }
-
-    let (biggest_i, biggest_digit) = biggest_found.expect("No digit found");
-    let biggest_second = input[(biggest_i + 1)..]
-        .chars()
-        .map(|ch| ch.to_digit(10).unwrap() as u64)
-        .max()
-        .unwrap();
-
-    biggest_digit * 10 + biggest_second
 }
 
 fn maximize_joltage(input: &str, digit_count: u64) -> u64 {
@@ -70,8 +48,6 @@ fn maximize_joltage(input: &str, digit_count: u64) -> u64 {
         joltage += found_ch;
 
         min_i += found_i + 1;
-        // dbg!(found_i, found_ch);
-        // println!("Repositioned at {min_i} ({})", input.chars().nth(min_i).unwrap_or(' '));
     }
 
     joltage
