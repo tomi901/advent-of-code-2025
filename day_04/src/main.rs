7e75c57259841b1e2f7f477e7026d21a588dbb59
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2025-12-04 11:37:50 -0300

    Day 4 - Part 2

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use anyhow::{self, Context};
-use xmas::{direction::{DIRECTIONS_8, Direction}, display_result, map2d::{ByteMap, Map2D}, point2d::Point2D};
+use xmas::{direction::DIRECTIONS_8, display_result, map2d::ByteMap, point2d::Point2D};
 
 fn main() -> anyhow::Result<()> {
     part_1()?;
@@ -13,23 +13,40 @@ fn main() -> anyhow::Result<()> {
 fn part_1() -> anyhow::Result<()> {
     println!("Part 1:");
     let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
+    let map = ByteMap::from_str(&input).unwrap();
 
-    let result = get_accessible_count(&input);
-    display_result(&result);
+    let accessible = get_accessible(&map);
+    display_result(&accessible.len());
     Ok(())
 }
 
 fn part_2() -> anyhow::Result<()> {
     println!("Part 2:");
 
+    let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
+    let mut map = ByteMap::from_str(&input).unwrap();
+
+    let mut count = 0;
+    loop {
+        let accessible = get_accessible(&map);
+        if accessible.len() == 0 {
+            break;
+        }
+
+        count += accessible.len();
+        for point in accessible {
+            map.set_tile(point, b'.');
+        }
+    }
+
+    display_result(&count);
     Ok(())
 }
 
-fn get_accessible_count(input: &str) -> usize {
-    let map = ByteMap::from_str(input).unwrap();
+fn get_accessible(map: &ByteMap) -> Vec<Point2D> {
     const ROLL: u8 = b'@';
 
-    let mut count = 0;
+    let mut result = Vec::new();
     for (point, _) in map.iter_with_points().filter(|x| *x.1 == ROLL) {
         let roll_count = DIRECTIONS_8
             .map(|d| point + d)
@@ -38,9 +55,9 @@ fn get_accessible_count(input: &str) -> usize {
             .count();
 
         if roll_count < 4 {
-            count += 1;
+            result.push(point);
         }
     }
 
-    count
+    result
 }
